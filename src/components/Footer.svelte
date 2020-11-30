72107792e3e6a4e85fa6db24159a6195b5842019
LFN <liangfengning@foxmail.com>
2020-11-30 23:54:18 +0800

    fix: remove ICP link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,23 +12,6 @@
     line-height: 1.75rem;
     box-sizing: border-box;
   }
-
-  .de {
-    padding: 0 0.5rem;
-  }
-
-  a {
-    color: var(--font-secondary);
-    text-decoration: none;
-    transition: all 0.4s;
-  }
-
-  a:hover,
-  a:active,
-  a:focus {
-    color: var(--font-primary);
-  }
-
   @media (max-width: 426px) {
     footer {
       padding: 0 16px 16px 16px;
@@ -38,12 +21,4 @@
 
 <footer>
   <span> Made with ❤ by Lifeni</span>
-  <span class="de"> | </span>
-  <a
-    href="https://beian.miit.gov.cn/"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="github-link">
-    鲁ICP备19006085号
-  </a>
 </footer>
