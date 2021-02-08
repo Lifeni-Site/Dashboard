1274f6fc2579d5506a23748f76c6d38ea2ca657d
LFN <liangfengning@foxmail.com>
2021-02-08 12:02:18 +0800

    fix: fix utterances

--- src/components/Utterances.svelte
+++ src/components/Utterances.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
-  let show = false
   document.addEventListener('DOMContentLoaded', () => {
-    show = true
+    const comment = document.querySelector('#comment')
+    if (comment) {
+      const utterances = document.createElement('script')
+      utterances.setAttribute('src', 'https://utteranc.es/client.js')
+      utterances.setAttribute('repo', 'Lifeni-Site/Comment')
+      utterances.setAttribute('issue-term', 'dashboard/home')
+      utterances.setAttribute('theme', 'preferred-color-scheme')
+      utterances.setAttribute('crossOrigin', 'anonymous')
+      utterances.setAttribute('async', 'true')
+      comment.appendChild(utterances)
+    }
   })
 </script>
 
-{#if show}
-  <div class="comment" id="comment">
-    <script
-      src="https://utteranc.es/client.js"
-      repo="Lifeni-Site/Comment"
-      issue-term="dashboard/home"
-      theme="preferred-color-scheme"
-      crossorigin="anonymous"
-      async>
-    </script>
-  </div>
-{/if}
+<div class="comment" id="comment" />
 
 <style lang="less">
   .comment {
